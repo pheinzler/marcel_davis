d00de35d9609db28a306b88a4c448c89a59b5e58
paddy287 <patrick.heinzler@gmx.de>
2025-03-09 20:36:35 +0100

    use german weekdays

--- marcel_davis.py
+++ marcel_davis.py
@@ -72,7 +72,7 @@ def download_thm():
         today_menues = parse_menue(data)
         chache_datestr = datetime.now().strftime("%A")
         cache_date = datetime.now().strftime('%d.%m.%Y')
-        menue_cache = f"{chache_datestr} {cache_date}\n\n"
+        menue_cache = f"{weekday_dict[chache_datestr]} {cache_date}\n\n"
         for menue in today_menues:
             menue_cache += f"{menue}\n{today_menues[menue]}\n\n"
     with open(THM_FILENAME, 'w', encoding='utf-8') as file:
