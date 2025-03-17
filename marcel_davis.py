6692505cab22c152d1d4f3a07363bec0fa04ea32
paddy287 <patrick.heinzler@gmx.de>
2025-03-17 22:20:09 +0100

    cleanup and cache week menues

--- marcel_davis.py
+++ marcel_davis.py
@@ -28,8 +28,8 @@ with open('config.yaml', 'r') as file:
 
 TIMEOUT = conf["timeout"]
 
-THM_WEEK_FILENAME = conf["filename"]["thm_week"]
-UNIMA_WEEK_FILENAME = conf["filename"]["uma_week"]
+THM_WEEK_CACHE_KEY = conf["filename"]["thm_week_cache_key"]
+UNIMA_WEEK_CACHE_KEY = conf["filename"]["uma_week_cache_key"]
 ABO_FILENAME = conf["filename"]["abo"]
 CACHE_FILENAME = conf["filename"]["cache"]
 CACHE_TEMPLATE = conf["filename"]["cache_template"]
@@ -54,14 +54,18 @@ def set_up_cache():
     else:
         print(f"{CACHE_FILENAME} already exists.")
 
-def parse_menue(data)->dict:
-    """parse the json menue of a single day"""
-    menues = {}
-    # loop through json data and return dict of tpye {"menue category":"{meal} - {price}"}
-    for i in range(len(data)):
-        menue = f"{data[i]['name']} - {data[i]['prices']['students']}€"
-        menues[data[i]['category']] = menue
-    return menues
+def get_week_menue(cache_week:dict)->str:
+    menue_cache = f"Menue von Montag {cache_week['von']} bis Freitag {cache_week['bis']}\n\n"
+    try:
+        for day in weekday_dict.values():
+            menue_cache += f"{day} - {cache_week[day]['date']}\n"
+            for menue in cache_week[day]['day']:
+                menue_cache += f"- {menue}\n\t\t{cache_week[day]['day'][menue]}\n\n"
+            menue_cache += "\n"
+    except:
+        None
+    return menue_cache
+
 
 def download_thm():
     with open(CACHE_FILENAME, 'r') as file:
@@ -94,55 +98,49 @@ def download_thm():
         json.dump(cache, file, indent=4)
 
 
-def download_week(canteen_id:int, filename:str):
+def download_week(canteen_id:int, mensa_key:str):
     """cache the menue of this week of the canteen with the given id and write to .txt file"""
     # Find start of this weeks menue date and format as yyyy-mm-dd. On weekends get next weeks menue
+    with open(CACHE_FILENAME, 'r') as file:
+        cache = json.load(file)
     log.info(f"caching menue for mensa with id {canteen_id}.")
     date = datetime.now()
+    curr_date_time = date.strftime('%Y-%m-%d %H:%M:%S')
     datestr = date.strftime("%A")
     offset = days_to_sunday[datestr]
     date = datetime.now() + timedelta(offset) # sunday of weeks menue
-    menue_week = {}
     # loop over week
     for i in range(1,6):
+        log.info(f"caching menue {i}/5.")
         curr_date = date + timedelta(i)
         curr_date_request_fromat = curr_date.strftime('%Y-%m-%d')
         # request menue from open mensa api
         url=f"https://openmensa.org/api/v2/canteens/{canteen_id}/days/{curr_date_request_fromat}/meals"
         response = requests.get(url)
         time.sleep(TIMEOUT)
-        if response.status_code != 200:
-            menue_week[curr_date.strftime("%A")] = {"ahhhh":"Nichts gefunden"}
-            log.error(f"request for {curr_date}  in download_thm_week failed. status code: {response.status_code}")
-            continue
-        try:
-            log.info(f"try reading response json")
-            data = response.json()
-            if data is None:
-                menue_cache = "Hochschulmensa hat zu 💩"
-                menue_week[curr_date.strftime("%A")] = {"ahhh":"Hochschulmensa hat zu 💩"}
-                continue
-        except:
-            log.error(f"error reading reponse json - writing no menue to cache")
-            menue_cache = "Hochschulmensa hat zu 💩"
-            menue_week[curr_date.strftime("%A")] = {"ahhh":"Hochschulmensa hat zu 💩"}
-            continue
-        #parse over the menue of the current day
-        menue_week[curr_date.strftime("%A")] = parse_menue(data)
-
-    menue_from = (date+timedelta(1)).strftime("%d.%m.%Y")
-    menue_to = (date+timedelta(5)).strftime("%d.%m.%Y")
-    menue_date = f"Menue from Monday {menue_from} to Friday {menue_to}\n"
-    with open(filename, 'w', encoding='utf-8') as file:
-        #write the date of this weeks menue
-        file.write(f"{menue_date}")
-        for day in menue_week:
-            file.write(f"\n{weekday_dict[day]}\n")
-            # loop over all menues of this day and write them to the cache
-            for menue in menue_week[day]:
-                menue_cache = ""
-                menue_cache += f"{menue}\n{menue_week[day][menue]}\n"
-                file.write(menue_cache)
+        weekday = weekday_dict[curr_date.strftime("%A")]
+        cache[mensa_key][weekday]["day"]= {}
+        if response.status_code == 200:
+            try:
+                for ele in response.json():
+                    menue = Menue(ele)
+                    cache[mensa_key][weekday]["day"][menue.categorie] = menue.get()
+                    cache[mensa_key][weekday]["status"] = response.status_code
+                    cache[mensa_key][weekday]["date"] = curr_date_request_fromat
+            except:
+                    cache[mensa_key][weekday]["day"]["no menue"] = "Hochschulmensa hat zu 💩"
+                    cache[mensa_key][weekday]["status"] = response.status_code
+                    cache[mensa_key][weekday]["date"] = curr_date_request_fromat
+        else:
+            log.error(f"request for mensa at date {curr_date_request_fromat} failed. status code: {response.status_code}")
+            cache[mensa_key][weekday]["day"]["no menue"] = "Hochschulmensa hat zu 💩"
+            cache[mensa_key][weekday]["status"] = response.status_code
+            cache[mensa_key][weekday]["date"] = curr_date_request_fromat
+        cache[mensa_key][weekday]["last_update"] = curr_date_time
+        cache[mensa_key]["von"] = (date+timedelta(1)).strftime("%d.%m.%Y")
+        cache[mensa_key]["bis"] = (date+timedelta(5)).strftime("%d.%m.%Y")
+        with open(CACHE_FILENAME, 'w') as file:
+            json.dump(cache, file, indent=4)
 
 
 def create_abos():
@@ -153,9 +151,10 @@ def create_abos():
 def cache_all_menus():
     "caches all menus as files"
     log.info("caching menus")
-    download_week(CANTEEN_ID_THM, THM_WEEK_FILENAME)
     download_thm()
-    download_week(CANTEEN_ID_UMA, UNIMA_WEEK_FILENAME)
+    download_week(CANTEEN_ID_THM, THM_WEEK_CACHE_KEY)
+    download_week(CANTEEN_ID_UMA, UNIMA_WEEK_CACHE_KEY)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return_message = conf["messages"]["start"]
@@ -184,8 +183,9 @@ async def thm_week(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """return this weeks thm mensa menu"""
     log.info("thm_week was called")
     # Open the file and read its contents
-    with open(THM_WEEK_FILENAME, 'r') as file:
-        menue_cache = file.read()
+    with open(CACHE_FILENAME, 'r') as file:
+        cache = json.load(file)
+    menue_cache:str = get_week_menue(cache[THM_WEEK_CACHE_KEY])
     await context.bot.send_message(chat_id=update.effective_chat.id, text=menue_cache)
 
 
@@ -193,8 +193,10 @@ async def uni_week(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """return this weeks uni mensa menu"""
     log.info("uni_week was called")
     # Open the file and read its contents
-    with open(UNIMA_WEEK_FILENAME, 'r') as file:
-        menue_cache = file.read()
+    # with open(CACHE_FILENAME, 'r') as file:
+    #     cache = json.load(file)
+    # menue_cache = get_week_menue(cache[UNIMA_WEEK_CACHE_KEY])
+    menue_cache = "work in progress. Upadte coming - soon... maybe :)"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=menue_cache)
 
 
@@ -264,17 +266,18 @@ async def date(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 def send_all_abos():
-    bot = Bot(token=API_KEY)
-    all_abos = []
-    with open(ABO_FILENAME, 'r', encoding="utf-8") as abofile:
-        for line in abofile:
-            all_abos.append(line)
-    log.info(f"sending abos. currently there are {len(all_abos)} abos")
-    with open(THM_FILENAME, 'r', encoding="utf-8") as file:
-        menu = file.read()
-        if len(all_abos) > 0:
-            for chat_id in all_abos:
-                bot.send_message(chat_id=chat_id, text=menu)
+    None
+#     bot = Bot(token=API_KEY)
+#     all_abos = []
+#     with open(ABO_FILENAME, 'r', encoding="utf-8") as abofile:
+#         for line in abofile:
+#             all_abos.append(line)
+#     log.info(f"sending abos. currently there are {len(all_abos)} abos")
+#     with open(THM_FILENAME, 'r', encoding="utf-8") as file:
+#         menu = file.read()
+#         if len(all_abos) > 0:
+#             for chat_id in all_abos:
+#                 bot.send_message(chat_id=chat_id, text=menu)
 
 
 def run_scheduler():
