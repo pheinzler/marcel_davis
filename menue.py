e35b4d2be93ed963b550b6ea917b13988a5e265e
paddy287 <patrick.heinzler@gmx.de>
2025-03-18 18:09:47 +0100

    two decimal precision for menue prices

--- menue.py
+++ menue.py
@@ -3,8 +3,8 @@ class Menue:
     def __init__(self, data):
         self.categorie = data["category"]
         self.menue = data["name"]
-        self.price = data["prices"]["students"]
-        self.price_unit = "/100g" if "€ pro 100g Preis" in data["notes"] else ""
+        self.price = f"{data['prices']['students']:.2f}"
+        self.price_unit = "€/100g" if "pro 100g Preis" in data["notes"] else ""
 
     def print(self):
         print(f"{self.categorie}\n\n{self.menue} - {self.price}{self.price_unit}")
